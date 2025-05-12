af7cbabb5cac1d0d4d7755835ffda6a52fcf703c
Balakrishna-kini <balakrishnakini22@gmail.com>
2025-05-12 21:46:34 +0530

    Add working chat app with homepage

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
@@ -12,6 +13,18 @@ def index():
 @socketio.on('message')
 def handle_message(msg):
     print('Message received: ' + msg)
+    try:
+        # Try to parse as JSON to check if it's a typing indicator
+        data = json.loads(msg)
+        if 'type' in data and data['type'] == 'typing':
+            # Forward typing indicator to all clients
+            socketio.emit('typing', msg)
+            return
+    except:
+        # Not JSON or not a typing indicator, treat as regular message
+        pass
+    
+    # Forward the message to all clients
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
